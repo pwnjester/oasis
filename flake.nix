557087ba3f93e158feb247dcafbff82022b5079a
pwnjester <pythiaband@gmail.com>
2025-06-16 10:12:48 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Development environment for the Oasis project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    # TODO: Remove this once clay-renderers-small-fixes is merged
-    clay.url = "github:h4rldev/clay-renderers-small-fixes/pull1";
+    # TODO: Remove this once clay-nix is merged
+    clay.url = "github:h4rldev/clay-nix/pull1";
   };
 
   outputs = {
@@ -21,20 +21,19 @@
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           alejandra
-          gcc
-          ffmpeg-headless
           clang-tools
           bear
           pkg-config
-          raylib
           just
           clay.packages.${system}.default
         ];
 
-        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
-          pkgs.raylib
-          pkgs.ffmpeg-headless
-        ]}";
+        buildInputs = with pkgs; [
+          libbsd
+          ffmpeg-headless
+          raylib
+          miniaudio
+        ];
       };
     });
 }
